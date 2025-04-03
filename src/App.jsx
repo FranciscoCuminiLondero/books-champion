f2fd498e2ded78df1d537c87cd6e5359bc45f06c
FranciscoCumini <cuminifrancisco@gmail.com>
2025-04-03 20:15:30 -0300

    Add Books in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import BookItem from './components/bookItem/BookItem';
+import Books from './components/books/Books';
 
 function App() {
   const books = [
@@ -42,38 +43,7 @@ function App() {
     <>
       <h2>Book champions app</h2>
       <p>¡Quiero leer libros!</p>
-      <BookItem
-        title={books[0].title}
-        author={books[0].author}
-        rating={books[0].rating}
-        pageCount={books[0].pageCount}
-        imageUrl={books[0].imageUrl}
-        available={books[0].available}
-      />
-      <BookItem
-        title={books[1].title}
-        author={books[1].author}
-        rating={books[1].rating}
-        pageCount={books[1].pageCount}
-        imageUrl={books[1].imageUrl}
-        available={books[1].available}
-      />
-      <BookItem
-        title={books[2].title}
-        author={books[2].author}
-        rating={books[2].rating}
-        pageCount={books[2].pageCount}
-        imageUrl={books[2].imageUrl}
-        available={books[2].available}
-      />
-      <BookItem
-        title={books[3].title}
-        author={books[3].author}
-        rating={books[3].rating}
-        pageCount={books[3].pageCount}
-        imageUrl={books[3].imageUrl}
-        available={books[3].available}
-      />
+      <Books books={books} />
     </>
   );
 }
